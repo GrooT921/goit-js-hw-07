39a031ca482cfd87c8e8f589595fc18f93623095
GrooT921 <rarapin96@gmail.com>
2022-07-23 14:33:10 +0200

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryImg = document.querySelector('.gallery');
+const markupImg = createGalleryItem(galleryItems);
+
+galleryImg.insertAdjacentHTML('beforeend', markupImg);
+
+galleryImg.addEventListener('click', onGallaryImgClick);
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({preview , original, description})=>{
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+            </a>
+        </div>
+        `;
+    }).join('');
+}
+
+function onGallaryImgClick(evt) {
+    evt.preventDefault();
+
+  if (evt.target.classList.contains('gallery')) return;
+    const source = evt.target.dataset.source;
+
+    const instance = basicLightbox.create(`<img src="${source}" width="800" heigth="600">`,
+        {
+            onShow: instance => {
+                document.addEventListener('keydown', onEscapeBtn);
+            },
+            onClose: instance => {
+                document.removeEventListener('keydown', onEscapeBtn);
+            }
+        }
+    )
+    
+    instance.show();  
+
+    function onEscapeBtn(e) {
+    if (e.code === 'Escape') instance.close()
+    }
+}
+
+// console.log(galleryItems);
+// console.log(createGalleryItem(galleryItems))
